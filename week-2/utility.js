aba6ede327a8f0006a49d571859d99b71a2c428c
Mahdi Hasanzadeh <mahdi.h2619@gmail.com>
2025-11-11 21:40:01 +0430

    Implement isNumber function

--- week-2/utility.js
+++ week-2/utility.js
@@ -3,3 +3,8 @@ export function round(number, decimals = 2) {
   const FACTOR = 10 ** decimals;
   return Math.round(number * FACTOR) / FACTOR;
 }
+
+// return true if value is a number otherwise false
+export function isNumber(value) {
+  return typeof value == "number";
+}
